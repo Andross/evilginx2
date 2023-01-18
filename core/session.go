067e417456e920cbc94eb28597da0d15c04d4556
Blake Balick-Schreiber <blake.balick.schreiber@gmail.com>
2023-01-18 12:13:19 -0500

    Update session.go
    Add redirect variable to handle okta use case

--- core/session.go
+++ core/session.go
@@ -17,6 +17,7 @@ type Session struct {
 	IsAuthUrl     bool
 	IsForwarded   bool
 	RedirectCount int
+	NumRedirectErr int
 	PhishLure     *Lure
 }
 
@@ -33,6 +34,7 @@ func NewSession(name string) (*Session, error) {
 		IsAuthUrl:     false,
 		IsForwarded:   false,
 		RedirectCount: 0,
+		NumRedirectErr: 0,
 		PhishLure:     nil,
 	}
 	s.Tokens = make(map[string]map[string]*database.Token)
